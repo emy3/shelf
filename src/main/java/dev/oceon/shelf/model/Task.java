c6fe15963b552e310c9782dd286712233c50665e
emoceon <emoceon@gmail.com>
2025-01-14 21:14:56 +0000

    add task and repo

---
+++ src/main/java/dev/oceon/shelf/model/Task.java
@@ -0,0 +1,19 @@
+package dev.oceon.shelf.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private boolean completed;
+}
---
+++ src/main/java/dev/oceon/shelf/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package dev.oceon.shelf.repository;
+
+import dev.oceon.shelf.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
