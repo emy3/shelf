329e0133bdc9b9daa67f7b56ee5aa5d145d0d5cb
emoceon <emoceon@gmail.com>
2025-01-14 20:45:25 +0000

    initial commit

---
+++ src/test/java/dev/oceon/shelf/ShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oceon.shelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
